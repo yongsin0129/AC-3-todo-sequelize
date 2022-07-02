44ec366d25581b531d30cb4d73428b14ce50293e
yongsin0129 <yongsin0129@gmail.com>
2022-07-02 15:20:44 +0800

    feat : add login feature via passport

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const passport = require('passport')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
 const app = express()
 const PORT = 3000
 
@@ -11,16 +13,31 @@ const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(
+  session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+  })
+)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+const usePassport = require('./config/passport')
+
+usePassport(app)
 
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post(
+  '/users/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  })
+)
 
 app.get('/users/register', (req, res) => {
   res.render('register')
@@ -41,11 +58,13 @@ app.post('/users/register', (req, res) => {
     return bcrypt
       .genSalt(10)
       .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
+      .then(hash =>
+        User.create({
+          name,
+          email,
+          password: hash
+        })
+      )
       .then(() => res.redirect('/'))
       .catch(err => console.log(err))
   })
@@ -68,7 +87,7 @@ app.get('/', (req, res) => {
     nest: true
   })
     .then(todos => {
-      return res.render('index', { todos: todos })
+      return res.render('index', { todos })
     })
     .catch(error => {
       return res.status(422).json(error)
