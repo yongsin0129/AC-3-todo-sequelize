e52d9502abb7f43d0f61b3ccd4b3e2ad7996c6b5
yongsin0129 <yongsin0129@gmail.com>
2022-07-03 11:02:00 +0800

    feat : add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ const usePassport = require('./config/passport')
 
 usePassport(app)
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes)
 
 app.listen(PORT, () => {
