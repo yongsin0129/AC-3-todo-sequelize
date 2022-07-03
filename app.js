e0258c84aad8c374c73d745dfd0d13fe1ae8332f
yongsin0129 <yongsin0129@gmail.com>
2022-07-03 10:57:36 +0800

    refact : express router

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const passport = require('passport')
-const bcrypt = require('bcryptjs')
 const session = require('express-session')
+const routes = require('./routes')
 const app = express()
 const PORT = 3000
-
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -27,72 +22,7 @@ const usePassport = require('./config/passport')
 
 usePassport(app)
 
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post(
-  '/users/login',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/users/login'
-  })
-)
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash =>
-        User.create({
-          name,
-          email,
-          password: hash
-        })
-      )
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then(todos => {
-      return res.render('index', { todos })
-    })
-    .catch(error => {
-      return res.status(422).json(error)
-    })
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
